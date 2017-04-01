ff53936d7b3db7f0173fd6f35efc47b345e608aa
Félix Averlant <felixaverlant@Felixs-MacBook-Pro.local>
2017-04-01 17:41:35 -0400

    baseball-score-calendar

--- dist-cal/js/bundle.js
+++ dist-cal/js/bundle.js
@@ -1327,7 +1327,7 @@ $(document).ready(function() {
         no_results_text: "Oops, nothing found!"
     });
 
-    d3.csv('dist/data/teams.csv', function(err, csv) {
+    d3.csv('dist-cal/data/teams.csv', function(err, csv) {
         $.each(csv, function(i,v) {
             $(".chosen").append($("<option value=" + v.team_id + "></option>")
                 .text(v.loc_team_tx + ' ' + v.name_team_tx));
